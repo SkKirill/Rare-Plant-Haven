8b7aa917875704cdf8ab72d7504bbb1180b6dd87
SkKirill <kiruxa.37v@gmail.com>
2024-08-26 20:08:18 +0300

    Меняем систему на Linux

--- RarePlantHaven.API/Program.cs
+++ RarePlantHaven.API/Program.cs
@@ -10,21 +10,13 @@ namespace RarePlantHaven.API
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
+            /*
+                        app.UseHttpsRedirection();
+                        app.UseStaticFiles();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+                        app.UseRouting();
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
+                        app.UseAuthorization();*/
 
             app.MapControllerRoute(
                 name: "default",
